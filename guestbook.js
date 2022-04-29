a41ff881d1b30079b3867f139d660dc4acb8b576
simonabarankova <62140166+simonabarankova@users.noreply.github.com>
2022-04-29 17:09:50 +0200

    Update guestbook.js
    added path to API

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://633c5d4b.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
